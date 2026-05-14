0a1e5d8a4eb7cbb44e520371f9d8f1310c09d478
surousama <liyiuse2018@gmail.com>
2026-05-14 18:16:02 +0800

    chore: update workspace config and gameplay ability

--- twohearts/Source/twohearts/TwoHearts/Combat/Gameplay/Abilities/TwoHeartsGA_TestNormalAttack.cpp
+++ twohearts/Source/twohearts/TwoHearts/Combat/Gameplay/Abilities/TwoHeartsGA_TestNormalAttack.cpp
@@ -1,5 +1,7 @@
 #include "TwoHearts/Combat/Gameplay/Abilities/TwoHeartsGA_TestNormalAttack.h"
 
+#include "Abilities/GameplayAbility.h"
+#include "GameplayTagContainer.h"
 #include "TwoHearts/Combat/Gameplay/Tags/TwoHeartsGameplayTags.h"
 
 UTwoHeartsGA_TestNormalAttack::UTwoHeartsGA_TestNormalAttack()
@@ -8,8 +10,8 @@ UTwoHeartsGA_TestNormalAttack::UTwoHeartsGA_TestNormalAttack()
 	AssetTags.AddTag(FTwoHeartsGameplayTags::Ability_NormalAttack());
 	SetAssetTags(AssetTags);
 	ActivationOwnedTags.AddTag(FTwoHeartsGameplayTags::State_Action_NormalAttack());
-	BlockAbilitiesWithTag.AddTag(FTwoHeartsGameplayTags::State_CannotAttack());
-	BlockAbilitiesWithTag.AddTag(FTwoHeartsGameplayTags::State_CannotInput());
+	ActivationBlockedTags.AddTag(FTwoHeartsGameplayTags::State_CannotAttack());
+	ActivationBlockedTags.AddTag(FTwoHeartsGameplayTags::State_CannotInput());
 }
 
 void UTwoHeartsGA_TestNormalAttack::ActivateAbility(
